78406a9e1843112758bbc1e67981bcc89e450ae2
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2018-02-03 19:26:52 -0500

    tweets.py

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 import tweepy
 import csv
 import sys
+
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 consumer_key = 'Nri3o9wMEtl5OX4etF6qiM0L4'
@@ -12,17 +13,15 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Open/Create a file to append data
-#csvFile = open('tweets.csv', 'a')
-#Use csv Writer
-#csvWriter = csv.writer(csvFile)
-
-for tweet in tweepy.Cursor(api.search,q="#memo",count=100,\
-                           lang="en",\
-                           since_id=2017).items():
-
-##    if (tweet.place != None):
-##        print(str(tweet.created_at).translate(non_bmp_map), str(tweet.text).translate(non_bmp_map), str(tweet.coordinates))
-##        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'), tweet.coordinates])
-
-    print(str(tweet.text).translate(non_bmp_map).encode('utf-8'))
+q  = input("Enter a hashtag: ")
+if (q[0] != "#"):
+    q = "#" + str(q)
+    
+c = 0
+for tweet in tweepy.Cursor(api.search, q, count = 100, lang="en", since_id=2017).items(): #get tweets containing the input hashtag
+    
+    print(str(tweet.text).translate(non_bmp_map).encode('utf-8')) #print tweets
+    c = c + 1
+    
+    if (c == 500): # display the first 500 results
+        break
