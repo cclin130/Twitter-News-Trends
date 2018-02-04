070e56ad0138a0b43d643c0f2c5a4c85a870c357
cclin130 <cheng.lin103@gmail.com>
2018-02-03 20:24:13 -0500

    filter.py now counts words correctly

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 file = open('tweets.txt', 'r')
 stopwords = open('stopWords.txt', 'r')
 newfile = open('wordCounts.txt', 'w')
+wordList = [["", 0]]
 
 #function loops through line and calls check function on each word
 def checkLine(string):
@@ -10,17 +11,17 @@ def checkLine(string):
         if (string[x] >= 'a' and string[x] <= 'z'):
           temp +=string[x]
         else:
-            #print(temp)
-            checkWord(temp)
+            if (temp != " "):
+                checkWord(temp)
             temp = ""
-    #print(temp)
-    checkWord(temp)
+
+    if (temp != " "):
+        checkWord(temp)
 
 #function checks each word against existing database
 def checkWord(string):
     isNewString = True
     isStopWord = False
-    wordList = [["", 0]]
 
     #filter out stop words
     for line in stopwords:
@@ -32,14 +33,12 @@ def checkWord(string):
         # check if string is a new entry
         for x in range(len(wordList)):
             if string == wordList[x][0]:
-                wordList[x][1] +=1
-                #newfile.write
-                print(wordList[x][0] + " " + str(wordList[x][1]))
+                wordList[x][1] = wordList[x][1] + 1
+                #print(wordList[x][0] + " " + str(wordList[x][1]))
                 isNewString = False
         if isNewString == True:
             wordList.append([string, 1])
-            #newfile.write
-            #print(wordList[int(len(wordList)-1)][0] + " " + str(wordList[int(len(wordList)-1)][1]))
+            print(wordList[int(len(wordList)-1)][0] + " " + str(wordList[int(len(wordList)-1)][1]))
 
 #----------------main---------------
 for line in file:
